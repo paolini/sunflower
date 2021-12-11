400a5222c9cfc14d660d2dfa6b9a9e8f6a8bf484
Emanuele Paolini <emanuele.paolini@gmail.com>
2021-12-11 12:05:36 +0100

    fix animation

--- sunflower.js
+++ sunflower.js
@@ -6,30 +6,43 @@ var input_n = document.getElementById("input_n");
 var slider_n = document.getElementById("slide_n");
 var button_animate = document.getElementById("button_animate");
 var animate = false;
+var initial_theta = 0.3819660112501051; // 2-phi
+var theta;
+var n;
 
 function reset() {
-    var angle = 0.3819660112501051; // 2-phi
-    slider_theta.value = angle;
-    input_theta.value = angle;
+    theta = initial_theta;
+    n = 1000;
+    input_theta.value = theta;
+    input_n.value = n;
+    input_change();
+}
+
+function input_change() {
+    theta = parseFloat(input_theta.value);
+    n = parseInt(input_n.value);
+    slider_theta.value = input_theta.value;
+    slider_n.value = Math.log(input_n.value)/Math.log(1000);
     draw();
 }
 
 function slider_change() {
-    input_theta.value = slider_theta.value;
-    input_n.value = slider_n.value;
+    theta = parseFloat(slider_theta.value);
+    n = Math.round(Math.pow(1000, parseFloat(slider_n.value)));
+    input_n.value = n;
+    input_theta.value = theta;
     draw();
 }
+
 
 function toggle_animation() {
     animate = !animate;
     button_animate.value = animate ? "stop" : "animate";
-    draw();
+    update();
 }
 
 function draw() {   
     var R = canvas.height/2-5;
-    var theta = parseFloat(input_theta.value);
-    var n = Math.round(Math.pow(1000, parseInt(input_n.value)/1000));
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var center_x = canvas.width/2;
     var center_y = canvas.height/2;
@@ -48,12 +61,16 @@ function draw() {
         ctx.arc(x, y, 1+(n-i)*5/n, 0, 2 * Math.PI);
         ctx.fill();
     }
+}
+
+function update() {
+    draw();
     if (animate) {
         theta += 0.005 / n;
         if (theta>0.5) theta -= 0.5;
         input_theta.value = theta;
         slider_theta.value = theta;
-        window.requestAnimationFrame(draw);
+        window.requestAnimationFrame(update);
     }
 }
 
