dc9f1b9135592f3d58fbe3ad5bac9a789afb2dc6
Emanuele Paolini <emanuele.paolini@gmail.com>
2021-12-11 10:27:31 +0100

    * unica passata: costruisce il fiore dall'esterno * pulsante reset

--- sunflower.js
+++ sunflower.js
@@ -1,19 +1,36 @@
 var canvas = document.getElementById("mycanvas");
-
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "orange";
+var input_theta = document.getElementById("input_theta");
+
+function reset() {
+    input_theta.value=0.3819660112501051; // 2 - phi
+    input_theta.onchange();
+}
 
 function draw() {   
-    var n = parseInt(document.getElementById("input_n").value);
+    var R = canvas.height/2-5;
+    var theta = parseFloat(input_theta.value);
+    var n = Math.round(Math.pow(1000, parseInt(document.getElementById("input_n").value)/1000));
+    document.getElementById("span_theta").innerText = theta.toFixed(4);
+    document.getElementById("span_n").innerText = n;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var theta = (Math.sqrt(5)-1)/2;
+    var center_x = canvas.width/2;
+    var center_y = canvas.height/2;
     for(var i=0; i<n; i++) {
         var angle = i * theta * 2 * Math.PI;
-        var r = 0.2*i;
+        var r = R*(n-i)/(n+1);
+        var x = center_x + r * Math.cos(angle);
+        var y = center_y + r * Math.sin(angle);
+        ctx.strokeStyle = "black";              
         ctx.beginPath();
-        ctx.arc(320+ r * Math.cos(angle), 240 + r*Math.sin(angle), 2, 0, 2 * Math.PI);
+        ctx.moveTo(center_x, center_y)
+        ctx.lineTo(x,y);
+        ctx.stroke();
+        ctx.fillStyle = "orange";
+        ctx.beginPath();
+        ctx.arc(x, y, 1+(n-i)*5/n, 0, 2 * Math.PI);
         ctx.fill();
     }
 }
 
-draw();
+reset();
